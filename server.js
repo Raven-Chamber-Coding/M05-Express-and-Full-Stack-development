5d3d6b4a8506f78ef5fb7796afb04fa170680373
Lucas <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2022-11-22 03:34:43 +0000

    finished response object.

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const server = http.createServer((reg, res) => {
     console.log(req.url, req.method);
 
     //set header type content
-    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Type", "text/html");
 
-    res.write("Hello ninjas");
+    res.write(<head><link rel="stylesheet" href="#"></link></head>)
+    res.write("<p>Hello ninjas</p>");
+    res.write("<p>Hello again, ninjas</p>")
     res.end()
 });
 
