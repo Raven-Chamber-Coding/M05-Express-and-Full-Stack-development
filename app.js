1f8fcb9691f918ee92b98572b6a5517eac7778d3
Lucas <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2022-12-01 01:23:54 +0000

    fixed app

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
         {title: "Mario finds stars", snippet: "Lorem ipsum"},
         {title: "How to defeat Bowser", snippet: "Lorem ipsum"},
     ];
-    res.render("index", { title: "Home" });
+    res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
